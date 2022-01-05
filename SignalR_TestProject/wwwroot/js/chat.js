0ff5f244387c5ba96294488cff96f3b4e7519c36
Jamie McManus <91683790+jamie-mcmanus-gclinic@users.noreply.github.com>
2022-01-05 14:18:56 +0000

    Add project files.

---
+++ SignalR_TestProject/wwwroot/js/chat.js
@@ -0,0 +1,54 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/sampleHub").build();
+
+//Disable the send button until connection is established.
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    li.textContent = `${user} says ${message}`;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+
+connection.onclose(function () {
+    console.log(`Client Disconected at ${Date.now()}`);
+    let attempt = 1
+    setTimeout(function () {
+        connection.hub.start().then(function () {
+            console.log(`Client Reconnected after ${attempt} attempts at ${Date.now()} `);
+        }).catch(function (err) {
+            return console.error(err.toString());
+        });
+        attempt++;
+    }, 5000); // Restart connection after 5 seconds.
+});
+
+connection.onreconnecting(error => {
+    console.assert(connection.state === signalR.HubConnectionState.Reconnecting);
+
+    document.getElementById("messageInput").disabled = true;
+
+    const li = document.createElement("li");
+    li.textContent = `Connection lost due to error "${error}". Reconnecting.`;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
